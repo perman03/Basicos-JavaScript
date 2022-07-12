8d86a818c0ee3f5b249bcc4a77a9e68172b128d4
Franco Perez <frankperman03@gmail.com>
2022-07-11 22:49:40 -0500

    import y export

---
+++ src/basicos/08-imp-exp.js
@@ -0,0 +1,28 @@
+//IMPORT, EXPORT Y FUNCIONES COMUNES DE ARREGLOS
+
+//importamos el arreglo de 'heroes'
+import heroes, {owners} from '../data/heroes'   //heroes es el export por default (sin llaves) y owners es la export individual, lleva llaves
+
+console.log(heroes); //imprime el arreglo
+console.log(owners); //imprime las compañias 
+
+
+//FIND
+const getHeroeById = (id) => {
+    return heroes.find(heroe => heroe.id === id); //heroe = callback
+}
+
+console.log(getHeroeById(2)); //imprime el heroe con el ID 2 (spiderman)
+
+
+
+//FILTER
+const getHeroeByOwner = (owner) => {
+    return heroes.filter(heroe => heroe.owner === owner); 
+}
+
+console.log(getHeroeByOwner('DC'));  //imprime todos los heroes de DC
+
+
+
+
---
+++ src/data/heroes.js
@@ -0,0 +1,32 @@
+export const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
+];
+
+
+//EXPORTACIONES
+export const owners = ['DC', 'Marvel']; //exportacion individual
+export default heroes;  //exportacion por defecto
