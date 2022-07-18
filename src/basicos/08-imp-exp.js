c3d64720af30257e360beaa5aa2a1011a5c2e7db
Franco Perez <frankperman03@gmail.com>
2022-07-18 14:55:10 -0500

    promesas

--- src/basicos/08-imp-exp.js
+++ src/basicos/08-imp-exp.js
@@ -3,26 +3,25 @@
 //importamos el arreglo de 'heroes'
 import heroes, {owners} from '../data/heroes'   //heroes es el export por default (sin llaves) y owners es la export individual, lleva llaves
 
-console.log(heroes); //imprime el arreglo
-console.log(owners); //imprime las compañias 
+// console.log(heroes); //imprime el arreglo
+// console.log(owners); //imprime las compañias 
 
 
 //FIND
-const getHeroeById = (id) => {
+export const getHeroeById = (id) => {
     return heroes.find(heroe => heroe.id === id); //heroe = callback
 }
 
-console.log(getHeroeById(2)); //imprime el heroe con el ID 2 (spiderman)
+// console.log(getHeroeById(2)); //imprime el heroe con el ID 2 (spiderman)
 
 
 
 //FILTER
-const getHeroeByOwner = (owner) => {
+export const getHeroeByOwner = (owner) => {
     return heroes.filter(heroe => heroe.owner === owner); 
 }
 
-console.log(getHeroeByOwner('DC'));  //imprime todos los heroes de DC
-
+// console.log(getHeroeByOwner('DC'));  //imprime todos los heroes de DC
 
 
 
